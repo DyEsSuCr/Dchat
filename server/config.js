8248dc1eda1c383ba207fe01b2fe5543f18511ac
DylanSuarez <2000sajo@gmail.com>
2023-04-12 19:16:57 -0500

    userExtractor

--- server/config.js
+++ server/config.js
@@ -5,3 +5,4 @@ config()
 export const PORT = process.env.PORT
 export const HOST = process.env.HOST
 export const DIALECT = process.env.DIALECT
+export const SECRET = process.env.SECRET
---
+++ server/middlewares/userExtractor.js
@@ -0,0 +1,21 @@
+import { verify } from 'jsonwebtoken'
+import { SECRET } from '../config.js'
+
+export const userExtractor = async (req, res, next) => {
+  const authorization = req.headers.token
+  let token = ''
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    token = authorization.substring(7)
+  }
+
+  const decodedToken = verify(token, SECRET)
+
+  if (!token || !decodedToken) return res.status(401).json({ erro: 'token missing o invalid' })
+
+  const { id: userId } = decodedToken
+
+  req.userId = userId
+
+  next()
+}
