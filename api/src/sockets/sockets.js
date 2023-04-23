4e7be7e936bea0051490e6051bb4a4611b4494a2
DylanSuarez <2000sajo@gmail.com>
2023-04-22 19:55:35 -0500

    comment sockets routes

--- api/src/sockets/sockets.js
+++ api/src/sockets/sockets.js
@@ -5,3 +5,31 @@ export default (io) => {
     })
   })
 }
+
+/* import { Router } from 'express'
+import { User } from '../models/Users.js'
+
+export default (io) => {
+  const route = Router()
+
+  io.on('connection', (socket) => {
+    socket.on('message', (message) => {
+      console.log(message)
+    })
+  })
+
+  route.get('/users', async (req, res) => {
+    try {
+      const users = await User.findAll()
+
+      if (users.length <= 0) return res.status(200).json({ message: '0 users' })
+
+      res.status(200).json(users)
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  })
+
+  return route
+}
+ */
