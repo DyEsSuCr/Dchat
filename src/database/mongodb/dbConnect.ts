ad574a3bf7b385d4853392bc421f3f417df2d074
DylanSuarez <2000sajo@gmail.com>
2023-08-17 16:51:34 -0500

    chage: dbconnect
    console.log(err)

--- src/database/mongodb/dbConnect.ts
+++ src/database/mongodb/dbConnect.ts
@@ -1,5 +1,4 @@
 import config from '@/config/config'
-import { HTTPError } from '@/middlewares/error.handler'
 import mongoose from 'mongoose'
 
 export const dbConnect = async () => {
@@ -7,6 +6,6 @@ export const dbConnect = async () => {
     const db = await mongoose.connect(config.URI_MONGO)
     console.log(`🚀🚀 DB connected to ${db.connection.host} 🚀🚀`)
   } catch (err) {
-    throw new HTTPError(500, '⛔⛔ Failed Connection DB ⛔⛔')
+    console.log('⛔⛔ Failed Connection DB ⛔⛔')
   }
 }
