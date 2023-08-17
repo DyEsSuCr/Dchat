8e80637694961bc8816d96aa897cb4ad4116c024
DylanSuarez <2000sajo@gmail.com>
2023-08-17 18:24:17 -0500

    chage: remove middleware
    remove checkJwt middleware from routes auth/login

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
 import { AuthController } from './auth.controller'
-import { checkJwt } from '@/middlewares/check.jwt'
 
 const router = Router()
 
-router.post('/signin', checkJwt, AuthController.login)
+router.post('/signin', AuthController.login)
 router.post('/signup', AuthController.register)
 
 export { router }
