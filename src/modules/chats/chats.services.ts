14c0558a5f341c66caa51035ae3732ac32062eb3
DylanSuarez <2000sajo@gmail.com>
2023-08-21 12:57:07 -0500

    add: methods service chat

--- src/modules/chats/chats.services.ts
+++ src/modules/chats/chats.services.ts
@@ -41,4 +41,111 @@ export class ModelChat {
     )
     return FullChat
   }
+
+  static async fetchChats ({ user }: RequestExt) {
+    let results = await ChatModel.find({ users: { $elemMatch: { $eq: user?.id } } })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+      .populate('latestMessage')
+      .sort({ updatedAt: -1 })
+
+    results = await UserModel.populate(results, {
+      path: 'latestMessage.sender',
+      select: 'name pic email'
+    })
+
+    return results
+  }
+
+  static async createGroupChat ({ body, user }: RequestExt) {
+    // Please Fill all the feilds
+    if (!body.users || !body.name) throw new HTTPError(400, 'ALREADY_USER')
+
+    const users = JSON.parse(body.users)
+
+    // More than 2 users are required to form a group chat
+    if (users.length < 2) throw new HTTPError(400, 'ALREADY_USER')
+
+    users.push(user)
+
+    const groupChat = await ChatModel.create({
+      users,
+      chatName: body.name,
+      isGroupChat: true,
+      groupAdmin: user
+    })
+
+    const fullGroupChat = await ChatModel.findOne({ _id: groupChat._id })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+
+    return fullGroupChat
+  }
+
+  static async renameGroup ({ body }: RequestExt) {
+    const { chatId, chatName } = body
+
+    const updatedChat = await ChatModel.findByIdAndUpdate(
+      chatId,
+      {
+        chatName
+      },
+      {
+        new: true
+      }
+    )
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+
+    // Chat Not Found
+    if (!updatedChat) throw new HTTPError(404, 'ALREADY_USER')
+
+    return updatedChat
+  }
+
+  static async removeFromGroup ({ body }: RequestExt) {
+    const { chatId, userId } = body
+
+    // check if the requester is admin
+
+    const removed = await ChatModel.findByIdAndUpdate(
+      chatId,
+      {
+        $pull: { users: userId }
+      },
+      {
+        new: true
+      }
+    )
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+
+    // Chat Not Found
+    if (!removed) throw new HTTPError(404, 'ALREADY_USER')
+
+    return removed
+  }
+
+  static async addToGroup ({ body }: RequestExt) {
+    const { chatId, userId } = body
+
+    // check if the requester is admin
+
+    const added = await ChatModel.findByIdAndUpdate(
+      chatId,
+      {
+        $push: { users: userId }
+      },
+      {
+        new: true
+      }
+    )
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+
+    // Chat Not Found
+    if (!added) throw new HTTPError(404, 'ALREADY_USER')
+
+    return added
+  }
 }
