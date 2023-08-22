4d763e506a64b6f5ac059fbd4563132000f6c27d
DylanSuarez <2000sajo@gmail.com>
2023-08-22 14:11:49 -0500

    change: response login controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
       const { email, password } = body
       const logedUser = await ModelAuth.login({ email, password })
 
-      responseHandler(res, 200, { logedUser })
+      responseHandler(res, 200, logedUser)
     } catch (err) {
       next(err)
     }
@@ -19,7 +19,7 @@ export class AuthController {
       const { username, email, password } = body
       const registerUser = await ModelAuth.register({ username, email, password })
 
-      responseHandler(res, 201, { registerUser })
+      responseHandler(res, 201, registerUser)
     } catch (err) {
       next(err)
     }
