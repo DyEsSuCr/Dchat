aeac0cdc85db448e0dd5bf06f28fdace88b7721c
DylanSuarez <2000sajo@gmail.com>
2023-09-02 23:48:36 -0500

    redirec page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,69 +1,20 @@
 'use client'
 
-import { SyntheticEvent, useEffect, useState } from 'react'
-import io from 'socket.io-client'
-
-const socket = io('http://localhost:5173')
-
-interface Messages {
-  body: string
-  from: string
-}
+import { SigninForm } from '@/components/signin'
+import { useAuthContext } from '@/hooks/useAuthContext'
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function App () {
-  const [messages, setMessages] = useState<Messages[]>([])
-  const [message, setMessage] = useState<string>('')
+  const { isAuthenticated } = useAuthContext()
 
   useEffect(() => {
-    socket.on('message', receiveMessage)
-
-    return () => {
-      socket.off('message', receiveMessage)
-    }
-  }, [])
-
-  const receiveMessage = (message: Messages) => setMessages(state => [message, ...state])
-
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
-
-    const newMessage: Messages = {
-      body: message,
-      from: 'Me'
-    }
-
-    setMessages(state => [newMessage, ...state])
-    setMessage('')
-
-    socket.emit('message', newMessage.body)
-  }
+    if (isAuthenticated) return redirect('chat')
+  }, [isAuthenticated])
 
   return (
-    <div className='h-screen bg-zinc-800 text-white flex items-center justify-center'>
-      <form onSubmit={handleSubmit} className='bg-zinc-900 p-10'>
-        <h1 className='text-2xl font-bold my-2'>Chat React</h1>
-        <input
-          name='message'
-          type='text'
-          placeholder='Write your message...'
-          onChange={(e) => setMessage(e.target.value)}
-          className='border-2 border-zinc-500 p-2 w-full text-black'
-          value={message}
-          autoFocus
-        />
-
-        <ul className='h-80 overflow-y-auto'>
-          {messages.map((message, index) => (
-            <li
-              key={index}
-              className={`my-2 p-2 table text-sm rounded-md ${message.from === 'Me' ? 'bg-sky-700 ml-auto' : 'bg-black'
-                }`}
-            >
-              <b>{message.from}</b>:{message.body}
-            </li>
-          ))}
-        </ul>
-      </form>
+    <div>
+      <SigninForm />
     </div>
   )
 }
